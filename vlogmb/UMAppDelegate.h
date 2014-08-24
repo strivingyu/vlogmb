d9df1e3bc7528a4e2a004fee3e30c4cf20075211
余少勇 <ysy@yushaoyongdeMacBook-Pro.local>
2014-08-24 19:43:20 +0800

    Initial Commit

---
+++ vlogmb/UMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UMAppDelegate.h
+//  vlogmb
+//
+//  Created by 余少勇 on 14-8-24.
+//  Copyright (c) 2014年 you-meng.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ vlogmb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  vlogmb
+//
+//  Created by 余少勇 on 14-8-24.
+//  Copyright (c) 2014年 you-meng.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UMAppDelegate class]));
+    }
+}
